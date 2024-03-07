72a3e027caccac5153cd9c9f9b31b2ed5627e6ed
Pablo Valdes <pablovaldes0925@gmail.com>
2024-03-07 14:10:52 -0500

    project section changes

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -34,9 +34,9 @@ const ProjectSection = ( props: ProjectProps ) => {
             <Image src={`/${portfolioSiteFileName}`} width={700} height={350} alt="chess icon" onClick={props.toggleDarkMode} className='cursor-pointer border-2 border-black shadow-lg shadow-black dark:shadow-white'/>
           </div>
           <div className='max-[1420px]:w-full flex max-[1420px]:items-center max-[1420px]:pt-8 flex-col items-start gap-4 roboto-medium w-1/2 px-12'>
-            <h1 className='text-black dark:text-white text-4xl'>My Portfolio</h1>
-            <p className='max-[1420px]:w-auto flex max-[1420px]:text-balance max-[1420px]:text-center text-black dark:text-white text-2xl w-1/2'>A culmination of my skills & growing confidence as a Full Stack Web Developer.</p>
-            <div className='flex flex-row flex-wrap gap-3 text-xl'>
+            <h1 className='max-[430px]:text-2xl max-[450px]:text-center text-black dark:text-white text-4xl'>My Portfolio</h1>
+            <p className='max-[430px]:text-xl max-[1420px]:w-auto flex max-[1420px]:text-balance max-[1420px]:text-center text-black dark:text-white text-2xl w-4/5'>A culmination of my skills & growing confidence as a Full Stack Web Developer.</p>
+            <div className='flex flex-row flex-wrap justify-center gap-3 text-xl'>
               <div className='border-2 rounded-full border-transparent py-1 px-3 background text-white hover:scale-110'>
                 Next.js
               </div>
@@ -47,25 +47,27 @@ const ProjectSection = ( props: ProjectProps ) => {
                 Typescript
               </div>
             </div>
-            <div className='flex flex-row text-xl gap-4'>
+            <div className='flex max-[430px]:flex-col max-[430px]:items-center flex-row text-xl gap-4'>
               <Link href="/" className='btn-transition-light dark:btn-transition-dark border-2 rounded-sm bg-transparent py-1 pl-3.5 pr-4 hover:scale-110 text-black dark:text-white border-black dark:border-white relative'>Details
               {arrow}
               </Link>
+              <div className='flex flex-row text-xl gap-4 items-center'>
               <Link href="/" >
                 <Image src={`/${githubIconFileName}`} width={30} height={30} alt="github icon" className='hover:scale-125' />
               </Link>
               <Link href="/" >
                 <Image src={`/${eyeIconFileName}`} width={30} height={30} alt="github icon" className='hover:scale-125' />
               </Link>
+              </div>
             </div>
           </div>
         </div>
         {/* Project 2 */}
         <div className='flex max-[1420px]:flex-col-reverse max-[1420px]:justify-center h-auto w-full flex-row my-4 p-8 light-card dark:background-card rounded-lg'>
           <div className='max-[1420px]:w-full flex max-[1420px]:items-center max-[1420px]:pt-8 flex-col items-start gap-4 roboto-medium w-1/2 px-4'>
-            <h1 className='text-black dark:text-white text-4xl'>iServer</h1>
-            <p className='max-[1420px]:w-auto flex max-[1420px]:text-balance max-[1420px]:text-center text-black dark:text-white text-2xl w-4/5'>An image uploading website with third party auth, design inspired by imgbb.com. Made to statically serve image urls for websites.</p>
-            <div className='flex flex-row flex-wrap gap-3 text-xl'>
+            <h1 className='max-[430px]:text-2xl text-black dark:text-white text-4xl'>iServer</h1>
+            <p className='max-[430px]:text-xl max-[1420px]:w-auto flex max-[1420px]:text-balance max-[1420px]:text-center text-black dark:text-white text-2xl w-4/5'>An image uploading website with third party auth, design inspired by imgbb.com. Made to statically serve image urls for websites.</p>
+            <div className='flex flex-row flex-wrap justify-center gap-3 text-xl'>
               <div className='border-2 rounded-full border-transparent py-1 px-3 background text-white hover:scale-110'>
                 Next.js
               </div>
@@ -82,16 +84,18 @@ const ProjectSection = ( props: ProjectProps ) => {
                 Clerk
               </div>
             </div>
-            <div className='flex flex-row items-center text-xl gap-4'>
+            <div className='flex max-[430px]:flex-col max-[430px]:items-center flex-row text-xl gap-4'>
               <Link href="/" className='btn-transition-light dark:btn-transition-dark border-2 rounded-sm bg-transparent py-1 pl-3.5 pr-4 hover:scale-110 text-black dark:text-white border-black dark:border-white relative'>Details
               {arrow}
               </Link>
+              <div className='flex flex-row text-xl gap-4 items-center'>
               <Link href="/" >
                 <Image src={`/${githubIconFileName}`} width={30} height={30} alt="github icon" className='hover:scale-125' />
               </Link>
               <Link href="/" >
                 <Image src={`/${eyeIconFileName}`} width={30} height={30} alt="github icon" className='hover:scale-125' />
               </Link>
+              </div>
             </div>
           </div>
           <div className='max-[1420px]:w-auto flex max-[1420px]:justify-center w-1/2 items-center'>
